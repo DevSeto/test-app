68d3ac6c0b115beaab6f0f9a1d0bae42b32a7847
sgevorgyan <sgevorgyan@cloudlinux.com>
2020-02-25 03:48:22 +0400

    update

--- db.sql
+++ db.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Хост: localhost
--- Время создания: Фев 25 2020 г., 01:52
+-- Время создания: Фев 25 2020 г., 02:20
 -- Версия сервера: 5.7.29-0ubuntu0.16.04.1
 -- Версия PHP: 7.0.33-0ubuntu0.16.04.12
 
@@ -31,11 +31,34 @@ CREATE TABLE `tasks` (
   `email` varchar(100) NOT NULL DEFAULT '',
   `name` varchar(100) DEFAULT NULL,
   `status` enum('0','1') DEFAULT '0',
-  `edit_at` enum('0','1') DEFAULT '0',
   `body` text,
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `edit_at` varchar(22) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `username` varchar(50) NOT NULL DEFAULT '',
+  `email` varchar(100) NOT NULL DEFAULT '',
+  `password` varchar(255) NOT NULL DEFAULT '',
+  `admin` tinyint(1) NOT NULL DEFAULT '0',
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `admin`, `created_at`, `updated_at`) VALUES
+(1, 'admin', 'admin@admin.com', '$2y$10$EMGmxGZM5yYWYl0cgozat.FKXkZ2CJKyI.CKxclRMDpQRM9tInpZy', 1, '2016-09-25 00:58:56', '2016-09-30 22:34:50');
 
 --
 -- Индексы сохранённых таблиц
@@ -48,6 +71,14 @@ ALTER TABLE `tasks`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_username_unique` (`username`),
+  ADD UNIQUE KEY `users_email_unique` (`email`);
+
+--
 -- AUTO_INCREMENT для сохранённых таблиц
 --
 
@@ -56,6 +87,11 @@ ALTER TABLE `tasks`
 --
 ALTER TABLE `tasks`
   MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
